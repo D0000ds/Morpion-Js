ec65cdbb2e116a5b79570335efa576c7e0ee2050
D0000ds <93730587+D0000ds@users.noreply.github.com>
2023-07-05 16:55:20 +0200

    exo morpion js

--- Morpion JS/Js/script.js
+++ Morpion JS/Js/script.js
@@ -1,5 +1,5 @@
 function saisieValeurInputJ1(){
-    let inputSaisie = document.getElementById("popUpPseudoInput").value
+    inputSaisie = document.getElementById("popUpPseudoInput").value
     localStorage.setItem("pseudo", inputSaisie)
 
     let popUpPseudoDel = document.getElementsByClassName("popUpPseudo")
@@ -10,7 +10,13 @@ function saisieValeurInputJ1(){
 
 function saisieValeurInputJ2(){
     let inputSaisie2 = document.getElementById("popUpPseudoInput").value
-    localStorage.setItem("pseudo2", inputSaisie2)
+    if(inputSaisie2 == localStorage.getItem("pseudo")){
+        alert("Vous avez choisie le même pseudo que le Joueur 1 veuillez changer s'il vous plaît.")
+        return
+    }
+    else{
+        localStorage.setItem("pseudo2", inputSaisie2)
+    }
 
     let popUpPseudoDel2 = document.getElementsByClassName("popUpPseudo")
     popUpPseudoDel2[0].remove()
@@ -19,27 +25,41 @@ function saisieValeurInputJ2(){
 }
 
 function popUpJ1(){
-    let container = document.querySelector(".container")
+    const container = document.querySelector(".container")
     
-    let popUpPseudo = document.createElement("div")
+    const popUpPseudo = document.createElement("div")
     popUpPseudo.classList.add("popUpPseudo")
     container.appendChild(popUpPseudo)
     
-    let popUpPseudoSpan = document.createElement("span")
+    const popUpPseudoSpan = document.createElement("span")
     popUpPseudoSpan.setAttribute("id", "popUpPseudoSpan")
     popUpPseudo.appendChild(popUpPseudoSpan)
     
     document.getElementById("popUpPseudoSpan").innerHTML = "joueur 1 veuillez choisir votre pseudo"
     
-    let popUpPseudoForm = document.createElement("form")
+    const popUpPseudoForm = document.createElement("form")
     popUpPseudoForm.setAttribute("id", "popUpPseudoForm")
     popUpPseudo.appendChild(popUpPseudoForm)
     
-    let popUpPseudoInput = document.createElement("input")
+    const popUpPseudoInput = document.createElement("input")
     popUpPseudoInput.setAttribute("id", "popUpPseudoInput")
     popUpPseudoForm.appendChild(popUpPseudoInput)
     
-    let popUpPseudoButton = document.createElement("button")
+    const popUpPseudoSpanSymbole = document.createElement("span")
+    popUpPseudoSpanSymbole.setAttribute("id", "popUpPseudoSpanSymbole")
+    popUpPseudo.appendChild(popUpPseudoSpanSymbole)
+    
+    document.getElementById("popUpPseudoSpanSymbole").innerHTML = "Votre Symbole sera:"
+
+    const popUpPseudoFig = document.createElement("figure")
+    popUpPseudoFig.setAttribute("id", "popUpPseudoFig")
+    popUpPseudo.appendChild(popUpPseudoFig)
+
+    const popUpImg = document.createElement("img")
+    popUpImg.setAttribute("src", "./img/x.png")
+    popUpPseudoFig.appendChild(popUpImg)
+
+    const popUpPseudoButton = document.createElement("button")
     popUpPseudoButton.setAttribute("id", "popUpPseudoButton")
     popUpPseudo.appendChild(popUpPseudoButton)
     popUpPseudoButton.onclick = saisieValeurInputJ1
@@ -47,35 +67,96 @@ function popUpJ1(){
 }
 
 function popUpJ2(){
-    let container = document.querySelector(".container")
+    const container = document.querySelector(".container")
     
-    let popUpPseudo = document.createElement("div")
+    const popUpPseudo = document.createElement("div")
     popUpPseudo.classList.add("popUpPseudo")
     container.appendChild(popUpPseudo)
     
-    let popUpPseudoSpan = document.createElement("span")
+    const popUpPseudoSpan = document.createElement("span")
     popUpPseudoSpan.setAttribute("id", "popUpPseudoSpan")
     popUpPseudo.appendChild(popUpPseudoSpan)
     
     document.getElementById("popUpPseudoSpan").innerHTML = "joueur 2 veuillez choisir votre pseudo"
     
-    let popUpPseudoForm = document.createElement("form")
+    const popUpPseudoForm = document.createElement("form")
     popUpPseudoForm.setAttribute("id", "popUpPseudoForm")
     popUpPseudo.appendChild(popUpPseudoForm)
     
-    let popUpPseudoInput = document.createElement("input")
+    const popUpPseudoInput = document.createElement("input")
     popUpPseudoInput.setAttribute("id", "popUpPseudoInput")
     popUpPseudoForm.appendChild(popUpPseudoInput)
+
+    const popUpPseudoSpanSymbole = document.createElement("span")
+    popUpPseudoSpanSymbole.setAttribute("id", "popUpPseudoSpanSymbole")
+    popUpPseudo.appendChild(popUpPseudoSpanSymbole)
     
-    let popUpPseudoButton = document.createElement("button")
+    document.getElementById("popUpPseudoSpanSymbole").innerHTML = "Votre Symbole sera:"
+
+    const popUpPseudoFig = document.createElement("figure")
+    popUpPseudoFig.setAttribute("id", "popUpPseudoFig")
+    popUpPseudo.appendChild(popUpPseudoFig)
+
+    const popUpImg = document.createElement("img")
+    popUpImg.setAttribute("src", "./img/letter-o.png")
+    popUpPseudoFig.appendChild(popUpImg)
+
+    
+    const popUpPseudoButton = document.createElement("button")
     popUpPseudoButton.setAttribute("id", "popUpPseudoButton")
     popUpPseudo.appendChild(popUpPseudoButton)
     popUpPseudoButton.onclick = saisieValeurInputJ2
     popUpPseudoButton.innerText = "jouer"
 }
 
+function victoire(numeroCaseVic, turnVic){
+    
+}
+
 function start(){
     document.getElementById("pseudoTour").innerHTML = localStorage.getItem("pseudo")
+    
+    const figSymbole = document.querySelector(".figureTurnSymbole")
+
+    const turnSymbole = document.createElement("img")
+    turnSymbole.setAttribute("src", "./img/x.png")
+    figSymbole.appendChild(turnSymbole)
+
+    let turn = 0
+
+    for(let i = 1; i <= 9; i++){
+        const box = document.querySelector("#box"+i)
+
+
+        box.addEventListener("click", function(){
+
+            if(turn == 0){ 
+                if(box.classList.contains("clicked-O") || box.classList.contains("clicked-X")){ 
+                    return
+                }
+                else{
+                    box.classList.add("clicked-X")
+                    document.getElementById("pseudoTour").innerHTML = localStorage.getItem("pseudo2")
+                    turnSymbole.setAttribute("src", "./img/letter-o.png")
+                    turn++
+                    victoire(i, turn)
+                }
+
+            }
+            else{
+                if(box.classList.contains("clicked-X") || box.classList.contains("clicked-O") ){
+                    return
+                }
+                else{
+                    box.classList.add("clicked-O")
+                    document.getElementById("pseudoTour").innerHTML = localStorage.getItem("pseudo")
+                    turnSymbole.setAttribute("src", "./img/x.png")
+                    turn--
+                }
+            }
+           
+        })
+    }
 }
 
 
